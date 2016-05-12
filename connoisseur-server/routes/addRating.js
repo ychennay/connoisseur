041b7c6230654658a737542cb53c434909a1fdc5
Kevin <kevlu94@gmail.com>
2016-05-12 16:47:22 -0700

    Fixed addRating route to update both the users table and the ratings table.

--- connoisseur-server/routes/addRating.js
+++ connoisseur-server/routes/addRating.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var Rating = require('../models/rating');
+var User = require('../models/user');
 
-router.post('/', function(req, res, next) {
+router.post('/', passport.authenticate('jwt', { session: false }), function(req, res) {
+    
+    var restaurantId = req.body.restaurantId;
+    var username = req.user.username;
+    var rating = req.body.rating;
 
-    new Rating({
-        restaurantId: req.body.restaurantId,
-        username: req.body.username,
-        rating: req.body.rating,
-        created_at: Date.now(),
-        updated_at: Date.now()
-    }).save( function(err) {
+    User.findOneAndUpdate({
+        username: username
+    }, {
+        $push: {
+            ratings: {
+                restaurantId: restaurantId,
+                rating: rating
+            }
+        }
+    }, {
+        upsert: false
+    }, function(err) {
+        new Rating({
+            restaurantId: restaurantId,
+            username: username,
+            rating: rating,
+            created_at: Date.now(),
+            updated_at: Date.now()
+        }).save( function(err) {
             if (err) {
                 res.sendStatus(400);
                 console.log(err);
@@ -19,6 +37,7 @@ router.post('/', function(req, res, next) {
                 res.sendStatus(200);
             }
         });
+    });
 });
 
 module.exports = router;
