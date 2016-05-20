440d6405d34a42bc7d5ca0fca5215a3a689f2a22
derek <defoster22@ucla.edu>
2016-05-20 15:53:57 -0700

    remove files

--- scripts/start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-service mongod --dbpath data/ 
-service npm start
-
--- scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-isExistApp=`pgrep npm`
-if [[ -n  \$isExistApp ]]; then
-   service npm stop
-fi
-isExistApp=`pgrep mongod`
-if [[ -n  \$isExistApp ]]; then
-    service mongod stop
-fi
